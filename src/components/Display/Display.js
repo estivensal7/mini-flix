d996e3d9673a254bace6d2c145a7ef4dca3b6a45
estivensal7 <estivensal7@gmail.com>
2019-04-21 23:26:24 -0400

    used axios to get request for all videos

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -2,10 +2,30 @@ import React, { Component } from "react";
 import { Link } from "react-router";
 import Nav from "../Nav/Nav";
 import { isLoggedIn } from "../../utils/AuthService";
+import { CloudinaryContext, Transformation, Video } from "cloudinary-react";
 import axios from "axios";
+import { Cloudinary } from "cloudinary-core";
 
 class Display extends Component {
+	state = {
+		videos: []
+	};
+
+	getVideos() {
+		axios.get(
+			"http://res.cloudinary.com/dnnsgy24u/video/list/miniflix.json"
+		).then(res => {
+			console.log(res.data.resources);
+			this.setState({ videos: res.data.resources });
+		});
+	}
+
+	componentDidMount() {
+		this.getVideos();
+	}
+
 	render() {
+		const { videos } = this.state;
 		return (
 			<div>
 				<Nav />
@@ -15,7 +35,31 @@ class Display extends Component {
 				</h3>
 				<hr />
 
-				<div className="col-sm-12" />
+				<div className="col-sm-12">
+					<CloudinaryContext cloudName="dnnsgy24u">
+						{videos.map((data, index) => (
+							<div
+								className="col-sm-4"
+								key={index}
+							>
+								<div className="embed-responsive embed-responsive-4by3">
+									<Video
+										publicId={
+											data.public_id
+										}
+										width="300"
+										height="300"
+										controls
+									/>
+								</div>{" "}
+								Created at{" "}
+								{
+									data.created_at
+								}
+							</div>
+						))}
+					</CloudinaryContext>
+				</div>
 			</div>
 		);
 	}
