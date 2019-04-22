31c3bb342b7b625e439e695b3f1eaa2285203ef7
estivensal7 <estivensal7@gmail.com>
2019-04-22 00:17:24 -0400

    Updates

---
+++ precache-manifest.3192e30d58b5b54c490b9bc32d43edb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2320fde28442a5f6d535",
+    "url": "/mini-flix/static/js/runtime~main.0f63d28e.js"
+  },
+  {
+    "revision": "948271a834cc53859e1b",
+    "url": "/mini-flix/static/js/main.405b13c5.chunk.js"
+  },
+  {
+    "revision": "00119ff53393eaa16f3e",
+    "url": "/mini-flix/static/js/2.b78e9347.chunk.js"
+  },
+  {
+    "revision": "948271a834cc53859e1b",
+    "url": "/mini-flix/static/css/main.a6580f4e.chunk.css"
+  },
+  {
+    "revision": "f7dc95f3ae63f210461f21f6448f725e",
+    "url": "/mini-flix/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mini-flix/precache-manifest.3192e30d58b5b54c490b9bc32d43edb9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mini-flix/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
